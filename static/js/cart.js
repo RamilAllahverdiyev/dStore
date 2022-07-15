1503136eb7a30c2bda57f5e8a200a2e4895f1d5c
Ramil Allahverdiyev <allahverdiyevramil@yahoo.com>
2022-07-16 00:41:04 +0400

    part of payment implemented

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,19 +1,19 @@
-let updateBtns = document.getElementsByClassName('update-cart')
+var updateBtns = document.getElementsByClassName('update-cart')
 
-for(let i = 0; i < updateBtns.length; i++){
-    updateBtns[i].addEventListener('click', function(){
-        let productId = this.dataset.product
-        let action = this.dataset.action
-        console.log(productId + action)
-        console.log(user)
-        if(user === 'AnonymousUser'){
-            console.log("Not loged in")
-        }else{
-            updateUserOrder(productId, action)
-        }
-    })
+for (i = 0; i < updateBtns.length; i++) {
+	updateBtns[i].addEventListener('click', function(){
+		var productId = this.dataset.product
+		var action = this.dataset.action
+		console.log('productId:', productId, 'Action:', action)
+		console.log('USER:', user)
+
+		if (user == 'AnonymousUser'){
+			addCookieItem(productId, action)
+		}else{
+			updateUserOrder(productId, action)
+		}
+	})
 }
-
 
 function updateUserOrder(productId, action){
 	console.log('User is authenticated, sending data...')
@@ -32,7 +32,32 @@ function updateUserOrder(productId, action){
 		   return response.json();
 		})
 		.then((data) => {
-		    console.log(data)
-            location.reload()
+		    location.reload()
 		});
+}
+
+function addCookieItem(productId, action){
+	console.log('User is not authenticated')
+
+	if (action == 'add'){
+		if (cart[productId] == undefined){
+		cart[productId] = {'quantity':1}
+
+		}else{
+			cart[productId]['quantity'] += 1
+		}
+	}
+
+	if (action == 'remove'){
+		cart[productId]['quantity'] -= 1
+
+		if (cart[productId]['quantity'] <= 0){
+			console.log('Item should be deleted')
+			delete cart[productId];
+		}
+	}
+	console.log('CART:', cart)
+	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
+	
+	location.reload()
 }
